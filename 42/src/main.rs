be558d5505c6a63ae62b4e065e7b48ba8dbca247
--global <cct2223@gmail.com>
2025-10-10 12:08:06 -0700

    updating Sha 512

--- 42/src/main.rs
+++ 42/src/main.rs
@@ -1,14 +1,8 @@
 const H: [u64; 8] = [ 0x6a09e667f3bcc908,0xbb67ae8584caa73b, 0x3c6ef372fe94f82b,0xa54ff53a5f1d36f1,0x510e527fade682d1, 0x9b05688c2b3e6c1f, 0x1f83d9abfb41bd6b, 0x5be0cd19137e2179]; 
 
-//FIX
-const K: [u32; 64] = [0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
-   0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
-   0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
-   0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
-   0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
-   0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
-   0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
-   0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2];
+const K = [ 0x428a2f98d728ae22, 0x7137449123ef65cd, 0xb5c0fbcfec4d3b2f, 0xe9b5dba58189dbbc, 0x3956c25bf348b538, 0x59f111f1b605d019, 0x923f82a4af194f9b, 0xab1c5ed5da6d8118, 0xd807aa98a3030242, 0x12835b0145706fbe, 0x243185be4ee4b28c, 0x550c7dc3d5ffb4e2, 0x72be5d74f27b896f, 0x80deb1fe3b1696b1, 0x9bdc06a725c71235, 0xc19bf174cf692694, 0xe49b69c19ef14ad2, 0xefbe4786384f25e3, 0x0fc19dc68b8cd5b5, 0x240ca1cc77ac9c65, 0x2de92c6f592b0275, 0x4a7484aa6ea6e483, 0x5cb0a9dcbd41fbd4, 0x76f988da831153b5,0x983e5152ee66dfab, 0xa831c66d2db43210, 0xb00327c898fb213f, 0xbf597fc7beef0ee4, 0xc6e00bf33da88fc2, 0xd5a79147930aa725, 0x06ca6351e003826f, 0x142929670a0e6e70, 0x27b70a8546d22ffc, 0x2e1b21385c26c926, 0x4d2c6dfc5ac42aed, 0x53380d139d95b3df, 0x650a73548baf63de, 0x766a0abb3c77b2a8, 0x81c2c92e47edaee6, 0x92722c851482353b, 0xa2bfe8a14cf10364, 0xa81a664bbc423001, 0xc24b8b70d0f89791, 0xc76c51a30654be30, 0xd192e819d6ef5218, 0xd69906245565a910, 0xf40e35855771202a, 0x106aa07032bbd1b8, 0x19a4c116b8d2d0c8, 0x1e376c085141ab53, 0x2748774cdf8eeb99, 0x34b0bcb5e19b48a8, 0x391c0cb3c5c95a63, 0x4ed8aa4ae3418acb,0x5b9cca4f7763e373, 0x682e6ff3d6b2b8a3, 0x748f82ee5defb2fc, 0x78a5636f43172f60, 0x84c87814a1f0ab72, 0x8cc702081a6439ec, 0x90befffa23631e28, 0xa4506cebde82bde9, 0xbef9a3f7b2c67915, 0xc67178f2e372532b, 0xca273eceea26619c, 0xd186b8c721c0c207, 0xeada7dd6cde0eb1e, 0xf57d4f7fee6ed178, 0x06f067aa72176fba, 0x0a637dc5a2c898a6, 0x113f9804bef90dae, 0x1b710b35131c471b, 0x28db77f523047d84, 0x32caab7b40c72493, 0x3c9ebe0a15c9bebc, 0x431d67c49c100d4c, 0x4cc5d4becb3e42b6, 0x597f299cfc657e2a, 0x5fcb6fab3ad6faec, 0x6c44198c4a475817];
+
+use macros;
 
 
 fn main() {
@@ -24,7 +18,7 @@ fn main() {
     bytes.push(1);
     //K is a bytestring of the next multiple of 512 - (l+65) zeroes
     let bits = bytes.len()*8;
-    bytes.append( &mut vec![0; (1024-(bits+129)%1024)]);
+    bytes.append( &mut vec![0; 1024-(bits+129)%1024]);
 
 // Split into 1024 bit blocks
 // chunks is the number of times 1024 fits into length of bits, then add remainder/remainder spaces
@@ -32,14 +26,87 @@ fn main() {
     
     let num_chunks: u32 = (bytes.len()*8/1024) as u32;
     let mut chunked: Vec<Vec<u8>> = Vec::new();
-    let bytes_per_chunk: u32 = num_chunks/8; 
+    let bytes_per_chunk: u32 = num_chunks/8;
+    //chunking up the input into a vector
     for i in 0..num_chunks{
-        chunked.append(Vec::new());
+        chunked.push(Vec::new());
         for j in 0..bytes_per_chunk{
-            chunked[i].append(bytes[j]);
+            chunked[i as usize].push(bytes[j as usize]);
         }
     }  
+    
+    
+    //creating a message schedule array for each chunk and splitting it across the array
+    for chunk in num_chunks{
+        let mut message_schedule: [u64, 80] = [0;80];
+        let mut temp: Vec<u8> = Vec::new();
+        
+        for (i,j) in chunk.iter().enumerate(){
+            temp[i%8] = *j;
+            if (i%8) == 7{
+                message_schedule.push(u64::from_be_bytes(temp));
+            }
+            
+        }
 
+        temp.append(&mut vec![0;64]);
+
+    
+        //extending/rotating the bits i=in the rest of the array
+        for i in 16..79{
+            //let S0 = (a macros::rightrotate(38)) xor (a rightrotate 34) xor (a rightrotate 39)
+            //S1 := (e rightrotate 14) xor (e rightrotate 18) xor (e rightrotate 41)
+
+            let s0 = macros::choice((macros::rotright(message_schedule[i-15], 1)), (macros::rotright(message_schedule[i-15], 8)), (macros::rotright(message_schedule[i-15], 7)));
+            let s1 = macros::choice((macros::rotright(message_schedule[i-2], 19)), (macros::rotright(message_schedule[i-2], 61)), (macros::rotright(message_schedule[i-2], 6)));
+        
+            message_schedule[i] = message_schedule[i-16] + s0 + message_schedule[i-7] + s1;
+
+        }
+
+        let mut a = H[0];
+        let mut b = H[1];
+        let mut c = H[2];
+        let mut d = H[3];
+        let mut e = H[4];
+        let mut f = H[5];
+        let mut g = H[6];
+        let mut h = H[7];
+
+        for i in 0..79{
+            let s1 = macros::choice(macros::rotright(e,14), macros::rotright(e, 18), macros::rotright(e, 41));
+            let ch = macros::choice(macros::majority(e, f), (macros::majority(!e, f)));
+            let temp1 = h+s1+ch+K[i]+message_schedule[i];
+            let s0 = macros::choice(macros::rotright(a, 28), macros::rotright(a,34), macros::rotright(a, 39));
+            let maj = macros::choice(macros::majority(a,b), macros::majority(a,c), macros::majority(b,c));
+            let temp2 = s0+maj;
+
+            h = g;
+            g = f;
+            f = e;
+            e = d+temp1;
+            d = c;
+            c = b;
+            b = a;
+            a = temp1 + temp2;
+
+        }
+        
+        let h0 = H[0] + a;
+        let h1 = H[1] + b;
+        let h2 = H[2] + c;
+        let h3 = H[3] + d;
+        let h4 = H[4] + e;
+        let h5 = H[5] + f;
+        let h6 = H[6] + g;
+        let h7 = H[7] + h;
+
+        print!("{0x64b}", vec!([h0,h1,h2,h3,h4,h5,h6,h7];
+
+    
+
+
+    }
 
 
 }
