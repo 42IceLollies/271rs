e61ae99fc323aa0cf4f1601bf6f9fba027082d2b
--global <cct2223@gmail.com>
2025-10-22 14:25:09 -0700

    Started IX assignment

---
+++ 72/src/lib.rs
@@ -0,0 +1,16 @@
+pub struct ix(){
+
+}
+
+pub fn add_ix(x: &ix, y: &ix) -> f16{
+    //adds and returns two integer numbers
+}
+
+
+pub fn sub_ix(x: &ix, y: &ix){
+    //subtracts y from x and returns difference
+}
+
+pub fn mul_ix(x: &ix, y: &ix){
+
+}
