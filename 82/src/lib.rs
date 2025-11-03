6a09b3795475ce18bba93c9b16c71bd24c7809d4
--global <cct2223@gmail.com>
2025-11-02 20:04:34 -0800

    Ed25519 is done, albeit slightly buggy for right now

--- 82/src/lib.rs
+++ 82/src/lib.rs
@@ -49,7 +49,7 @@ const I: int = expmod(2, ((q-1)/4) as u8, q);
 // xrecover helper (nested for local use in setup and decode)
 pub fn xrecover(y: &BigInt, q: &BigInt, d: &BigInt, i_const: &BigInt) -> BigInt {
     let mut xx = (y*y - 1) * inv(d*y*y+1);
-    let mut x = expmod(xx, ((q+3)/8)as u8), q);
+    let mut x = expmod(xx, ((q+3)/8)as u8, q);
     if (x*x-xx)%q != 0 {
         x = (x*I)%q;
     }
@@ -145,18 +145,76 @@ pub fn publickey(sk: &[u8], b: usize, q: &BigInt, d: &BigInt, b_point: &Vec<BigI
 }
 
 fn hint(m: &[u8], b: usize) -> BigInt {
-    let h = H(m);
+    let h = h(m);
     let mut temp = Vec::new();
     for i in 0..2*b{
-        temp.append(2.pow(i)*bit(h,i);
+        temp.append(2.pow(i)*bit(h,i));
     }
     return temp.sum();
 }   
 
 pub fn signature(m: &[u8], sk: &[u8], pk: &[u8], b: usize, q: &BigInt, l: &BigInt, d: &BigInt, b_point: &Vec<BigInt>) -> Vec<u8> {
+    let h = h(sk);
+    let mut a = 2.pow(b-2);
+
+    for i in 3..(b-2){
+        a += 2.pow(i)*bit(h,i);
+    }
+
+    let r = hint(h[(b/8)as u8 .. (b/4) as u8]);
+    let R = scalarmult(B,r);
+    let h_sig = hint(encodepoint(R) + pk + m);
+    let S = (r+h_sig*a)%1;
+    return encodepoint(R) + encodein(S);
+}
 
 fn isoncurve(p: &Vec<BigInt>, q: &BigInt, d: &BigInt) -> bool {
+    let x = p[0];
+    let y = p[1];
+    return(-x*x+y*y-1-d*x*x*y*y) %  q == 0;
+}
 
 fn decodeint(s: &[u8], b: usize) -> BigInt {
+    let mut temp = Vec::new();
+    for i in 0..b{
+        temp.append(2.pow(i) * bit(s,i));
+    }
+    return temp.sum();
+
+}
+
+fn decodepoint(s: &u8, b: usize) -> BigInt {
+    let mut temp = Vec::new();
+    for i in 0..(b-1) { 
+        temp.append(2.pow(i) * bit(s,i));
+    }
+    let y = temp.sum();
+    let mut x = xrecover(y); 
+    if x&1 != bits(s, b-1){
+        x= q-x;
+    }
+    let P = [x,y];
+    
+    if !isoncurve(P){
+        panic!("decoding point that is not on curve");
+    }
+
+    return P;
+}
 
 pub fn checkvalid(s: &[u8], m: &[u8], pk: &[u8], b: usize, q: &BigInt, d: &BigInt, i_const: &BigInt, b_point: &Vec<BigInt>) -> bool {
+    if x.len() != (b/4) as u8{
+        panic!("signature length is wrong");
+    }
+    if pk.len() != (b/8) as u8{
+        panic!("public key length is wrong");
+    }
+
+    let R = decodepoint(s[0..(b/8) as usize]);
+    let A = decodepoint(pk);
+    let S = decodeint(s[(b/8)as usize .. (b/4) as usize]);
+    let h = Hint(encodepoint(R) + pk + m);
+    return scalarmult(B,S) == edwards(R, scalarmult(A, h));
+}
+
+
