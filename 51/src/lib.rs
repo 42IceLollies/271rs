d99d49f38c325975a069ea649e9e2c64a3146303
--global <cct2223@gmail.com>
2025-11-24 16:24:29 -0800

    started F16 lab

--- 51/src/lib.rs
+++ 51/src/lib.rs
@@ -1,3 +1,33 @@
-pub struct f16 { 
+pub struct F16 {
     bits: u16,
+    sign: u16,
+    exp: u16,
+    mantissa:u16,
+}
+
+pub fn f16(bits:u16, sign:u16, exp:u16, mantis:u16)-> F16 {
+    F16{
+        bits,
+        sign, 
+        exp,
+        mantis
+    }
+}
+
+pub fn i32_to_f16(n:i32) -> F16{
+    
+    F16{
+        //bits:
+        //just divide the int by two to find its bits and mantissa??? 
+        //should probably review the lecture for this :/
+    }
+}
+
+fn print_f16(x: F16){
+    print!("{}{}.{}*2^{}", x.sign, x.bits, x.mantissa,x.exponent );
+}
+
+fn println_f16(x:f16){
+    println!("{}{}.{}*2^{}", x.sign, x.bits, x.mantissa,x.exponent );
+
 }
--- 51/src/main.rs
+++ 51/src/main.rs
@@ -1,3 +1,7 @@
+//f16 (integer one)
+//erm actually, floats!
+//
+
 use fsixteen::f16;
 
 fn main() {
