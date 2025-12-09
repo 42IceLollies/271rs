ff490cb4c24becb065d7243250f5847e06b8afc5
--global <cct2223@gmail.com>
2025-12-09 14:08:46 -0800

    Finished Patchgit add E1

--- E1/src/lib.rs
+++ E1/src/lib.rs
@@ -6,7 +6,7 @@ use diff::read_into_vec;
 pub fn patch(diff: (Vec<(String, usize)>, Vec<(String, usize)>), file: String) -> String{
     let mut file_vec: Vec<String> = read_into_vec(&file).into_iter().map(|x| x.to_string()).collect();
      
-    dbg!(&file_vec);
+   
     for change in diff.0{
         file_vec.remove(change.1);
     }
@@ -23,14 +23,16 @@ pub fn unpatch(diff: (Vec<(String, usize)>, Vec<(String, usize)>), file: String)
     let mut file_vec: Vec<String> = read_into_vec(&file).into_iter().map(|x| x.to_string()).collect();
     
     //now the adds are removals
-    for i in diff.1.len()..0{
-        file_vec.remove(diff.1[i].1);
+    for i in (0..diff.1.len()).rev(){
+        file_vec.remove(diff.1[i].1);   
     }
 
 
     //and the removals are adds
-    for i in diff.0.len()..0{
+    for i in (0..diff.0.len()).rev(){
         file_vec.insert(diff.0[i].1, diff.0[i].0.clone());
     }
+
+    
     return  file_vec.into_iter().fold(String::new(), |mut acc, x| {acc.push_str(&x); acc});
 }
